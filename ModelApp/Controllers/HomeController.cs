08ee7033eed17a27ab3b90759a4bd19960c944c2
slovechik <y.melnyk@relevant.software>
2019-04-24 20:26:38 +0300

    first commit

---
+++ ModelApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelApp.Models;
+using ModelApp.ViewModels;
+
+namespace ModelApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        List<Company> companies;
+        List<Phone> phones;
+        public HomeController()
+        {
+            Company apple = new Company { Id = 1, Name = "Apple", Country = "США" };
+            Company microsoft = new Company { Id = 2, Name = "Microsoft", Country = "США" };
+            Company google = new Company { Id = 3, Name = "Google", Country = "США" };
+            companies = new List<Company> { apple, microsoft, google };
+
+            phones = new List<Phone>
+            {
+                new Phone { Id=1, Manufacturer= apple, Name="iPhone 6S", Price=56000 },
+                new Phone { Id=2, Manufacturer= apple, Name="iPhone 5S", Price=41000 },
+                new Phone { Id=3, Manufacturer= microsoft, Name="Lumia 550", Price=9000 },
+                new Phone { Id=4, Manufacturer= microsoft, Name="Lumia 950", Price=40000 },
+                new Phone { Id=5, Manufacturer= google, Name="Nexus 5X", Price=30000 },
+                new Phone { Id=6, Manufacturer= google, Name="Nexus 6P", Price=50000 }
+            };
+        }
+        //public IActionResult Index()
+        //{
+        //    return View(phones);
+        //}
+        public IActionResult Index(int? companyId)
+        {
+            // формируем список компаний для передачи в представление
+            List<CompanyModel> compModels = companies
+                .Select(c => new CompanyModel { Id = c.Id, Name = c.Name })
+                .ToList();
+            // добавляем на первое место
+            compModels.Insert(0, new CompanyModel { Id = 0, Name = "Все" });
+
+            IndexViewModel ivm = new IndexViewModel { Companies = compModels, Phones = phones };
+
+            // если передан id компании, фильтруем список
+            if (companyId != null && companyId > 0)
+                ivm.Phones = phones.Where(p => p.Manufacturer.Id == companyId);
+
+            return View(ivm);
+        }
+    }
+}
---
+++ ModelApp/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ModelApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelApp.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Phone> Phones { get; set; }
+        public IEnumerable<CompanyModel> Companies { get; set; }
+    }
+}
